00501796adb17b4e21dca936f237ab757e2b9d53
Jonathan Fok kan <jfokkan@gmail.com>
2016-02-19 00:58:03 -0500

    Added information gathering to background.js

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const SESSION_ALARM = 'session-saver';
+
+var pollingPeriodInMinutes = 1;
+
 /**
  * @param {Tab} tab - Gives the state of the tab that was updated.
  *
@@ -21,4 +25,25 @@ function onTabUpdated(tabId, changeInfo, tab) {
 }
 
 chrome.tabs.onCreated.addListener(onTabCreated);
-chrome.tabs.onUpdated.addListener(onTabUpdated)
+chrome.tabs.onUpdated.addListener(onTabUpdated);
+
+chrome.alarms.create(SESSION_ALARM, {periodInMinutes: pollingPeriodInMinutes});
+
+/*
+ * Periodically gather info on system and tabs
+ */
+function alarm (alarm) {
+  console.log(alarm);
+  if (alarm.name == SESSION_ALARM) {
+    chrome.system.cpu.getInfo(function (info) {
+      console.log(info);
+    });
+    chrome.system.memory.getInfo(function (info) {
+      console.log(info);
+    });
+    chrome.tabs.query({}, function (tabs) {
+      console.log(tabs);
+    })
+  }
+}
+chrome.alarms.onAlarm.addListener(alarm);
